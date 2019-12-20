2e889a32db5551b4b9694080fc6b9ef81c209797
Evans Njogu <safinasystems@gmail.com>
2019-12-20 23:29:03 +0300

    make

--- models/Notifications.js
+++ models/Notifications.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 var notificationSchema = mongoose.Schema({
     name:String,
-    details:String
+    details:String,
+    
 
 });
 var Notification = mongoose.model('Notification',notificationSchema);
